16ebc85670cc8ca4911d147eeea705684e883f8d
yeg2799 <yeg2799@gmail.com>
2022-02-17 23:09:30 +0300

    first commit

---
+++ layout/default.vue
@@ -0,0 +1,19 @@
+<template lang="pug">
+  .layout
+    Nuxt
+</template>
+
+<script>
+import { ref } from '@nuxtjs/composition-api';
+export default {
+  setup() {
+    const data = ref('emre');
+    return {
+      data
+    }
+  }
+}
+</script>
+
+<style>
+</style>
